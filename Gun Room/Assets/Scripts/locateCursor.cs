335768c96523637f20aa00ce883e9e5cab22d057
SirWretchedly <timotei.vodita@gmail.com>
2020-06-12 16:45:20 +0300

    Commit 1
    1. Mouse position detection in relation to player.
    2. Player idle animation in relation to mouse position.

---
+++ Gun Room/Assets/Scripts/locateCursor.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class locateCursor : MonoBehaviour
+{
+    public Vector3 playerPosition;
+    GameObject player;
+    Animator playerAnimator;
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        playerAnimator = player.GetComponent<Animator>();
+    }
+    void Update()
+    {
+        playerPosition = player.transform.position;
+        playerPosition = Camera.main.WorldToScreenPoint(playerPosition);
+        if (Input.mousePosition.y > playerPosition.y)
+        {
+            if(Input.mousePosition.x < (playerPosition.x- Screen.width / 3 / 2))
+            {
+                playerAnimator.SetInteger("mousePosition", 1);
+            }
+            else if(Input.mousePosition.x > (playerPosition.x + Screen.width / 3 / 2))
+            {
+                playerAnimator.SetInteger("mousePosition", 3);
+            }
+            else
+            {
+                playerAnimator.SetInteger("mousePosition", 2);
+            }
+        }
+        else
+        {
+            if (Input.mousePosition.x < (playerPosition.x - Screen.width / 3 / 2))
+            {
+                playerAnimator.SetInteger("mousePosition", 4);
+            }
+            else if (Input.mousePosition.x > (playerPosition.x + Screen.width / 3 / 2))
+            {
+                playerAnimator.SetInteger("mousePosition", 6);
+            }
+            else
+            {
+                playerAnimator.SetInteger("mousePosition", 5);
+            }
+        }
+    }
+}
