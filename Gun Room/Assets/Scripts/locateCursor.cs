077b147e031f7cb549ce8653f15a1afc21eb4c1f
SirWretchedly <timotei.vodita@gmail.com>
2020-06-12 22:24:51 +0300

    New Animation

--- Gun Room/Assets/Scripts/locateCursor.cs
+++ Gun Room/Assets/Scripts/locateCursor.cs
@@ -18,11 +18,11 @@ public class locateCursor : MonoBehaviour
         playerPosition = Camera.main.WorldToScreenPoint(playerPosition);
         if (Input.mousePosition.y > playerPosition.y)
         {
-            if(Input.mousePosition.x < (playerPosition.x- Screen.width / 3 / 2))
+            if(Input.mousePosition.x < (playerPosition.x- Screen.width / 9))
             {
                 playerAnimator.SetInteger("mousePosition", 1);
             }
-            else if(Input.mousePosition.x > (playerPosition.x + Screen.width / 3 / 2))
+            else if(Input.mousePosition.x > (playerPosition.x + Screen.width / 9))
             {
                 playerAnimator.SetInteger("mousePosition", 3);
             }
@@ -33,11 +33,11 @@ public class locateCursor : MonoBehaviour
         }
         else
         {
-            if (Input.mousePosition.x < (playerPosition.x - Screen.width / 3 / 2))
+            if (Input.mousePosition.x < (playerPosition.x - Screen.width / 9))
             {
                 playerAnimator.SetInteger("mousePosition", 4);
             }
-            else if (Input.mousePosition.x > (playerPosition.x + Screen.width / 3 / 2))
+            else if (Input.mousePosition.x > (playerPosition.x + Screen.width / 9))
             {
                 playerAnimator.SetInteger("mousePosition", 6);
             }
---
+++ Gun Room/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public float movementSpeed = 5;
+    float horizontal, vertical, movementLimit = 0.7f;
+    Animator playerAnimator;
+    Rigidbody2D body;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        playerAnimator = GameObject.FindWithTag("Player").GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        if(horizontal != 0 && vertical != 0)
+        {
+            horizontal *= movementLimit;
+            vertical *= movementLimit;
+        }
+        if(horizontal != 0 || vertical != 0)
+        {
+            playerAnimator.SetBool("moving", true);
+        }
+        else
+        {
+            playerAnimator.SetBool("moving", false);
+        }
+        body.velocity = new Vector2(horizontal * movementSpeed, vertical * movementSpeed);
+    }
+}
